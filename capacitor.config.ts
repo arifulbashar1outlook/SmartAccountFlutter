04592699b857366e7f000bd63c36c1d83f4817a3
arifulbashar1outlook <96435903+arifulbashar1outlook@users.noreply.github.com>
2025-12-14 17:54:11 +0000

    1

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartspend.tracker',
+  appName: 'SmartSpend Tracker',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
